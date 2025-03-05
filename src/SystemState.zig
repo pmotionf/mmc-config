3560cf0c728f7badbd497e3cd418167c03165e80
aaumar25 <aaumar@pmotionf.com>
2025-03-05 09:30:49 +0900

    refactor: update `Carrier.State`

--- src/SystemState.zig
+++ src/SystemState.zig
@@ -19,43 +19,38 @@ pub const Carrier = packed struct {
     state: State,
 
     pub const State = enum(u8) {
-        None = 0,
-        WarmupProgressing = 1,
-        WarmupCompleted = 2,
-        CurrentBiasProgressing = 4,
-        CurrentBiasCompleted = 5,
-        PosMoveProgressing = 29,
-        PosMoveCompleted = 30,
-        ForwardCalibrationProgressing = 32,
-        ForwardCalibrationCompleted = 33,
-        BackwardIsolationProgressing = 34,
-        BackwardIsolationCompleted = 35,
-        ForwardRestartProgressing = 36,
-        ForwardRestartCompleted = 37,
-        BackwardRestartProgressing = 38,
-        BackwardRestartCompleted = 39,
-        SpdMoveProgressing = 40,
-        SpdMoveCompleted = 41,
-        NextAxisAuxiliary = 43,
-        // Note: Next Axis Completed will show even when the next axis is
-        // progressing, if the carrier is paused for collision avoidance
-        // on the next axis.
-        NextAxisCompleted = 44,
-        PrevAxisAuxiliary = 45,
-        // Note: Prev Axis Completed will show even when the prev axis is
-        // progressing, if the carrier is paused for collision avoidance
-        // on the prev axis.
-        PrevAxisCompleted = 46,
-        ForwardIsolationProgressing = 47,
-        ForwardIsolationCompleted = 48,
-        Overcurrent = 50,
+        None = 0x0,
 
-        PullForward = 52,
-        PullForwardCompleted = 53,
-        PullBackward = 55,
-        PullBackwardCompleted = 56,
-        BackwardCalibrationProgressing = 58,
-        BackwardCalibrationCompleted = 59,
+        WarmupProgressing,
+        WarmupCompleted,
+
+        PosMoveProgressing = 0x4,
+        PosMoveCompleted,
+        SpdMoveProgressing,
+        SpdMoveCompleted,
+        Auxiliary,
+        AuxiliaryCompleted,
+
+        ForwardCalibrationProgressing = 0xA,
+        ForwardCalibrationCompleted,
+        BackwardCalibrationProgressing,
+        BackwardCalibrationCompleted,
+
+        ForwardIsolationProgressing = 0x10,
+        ForwardIsolationCompleted,
+        BackwardIsolationProgressing,
+        BackwardIsolationCompleted,
+        ForwardRestartProgressing,
+        ForwardRestartCompleted,
+        BackwardRestartProgressing,
+        BackwardRestartCompleted,
+
+        PullForward = 0x1A,
+        PullForwardCompleted,
+        PullBackward,
+        PullBackwardCompleted,
+
+        Overcurrent = 0x1F,
     };
 };
 
