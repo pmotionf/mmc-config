a62802a4ae1edc5723de9411620f222bdb90ae49
aaumar25 <aaumar@pmotionf.com>
2025-03-13 17:05:45 +0900

    refactor: `SystemState` has `command_status` field

--- src/SystemState.zig
+++ src/SystemState.zig
@@ -3,11 +3,13 @@ const Direction = @import("mmc-config.zig").Direction;
 const mcl = @import("mcl");
 const Line = mcl.Line;
 const Axis = mcl.Axis;
+const Station = mcl.Station;
 
 num_of_carriers: u10,
 num_of_active_axis: Axis.Id.Line,
 carriers: []Carrier,
 hall_sensors: []Hall,
+command_status: []CommandStatus,
 
 pub const Carrier = packed struct {
     line_id: Line.Id,
@@ -27,4 +29,10 @@ pub const Hall = packed struct {
         front: bool,
         back: bool,
     },
+};
+
+pub const CommandStatus = packed struct {
+    line_id: Line.Id,
+    station_id: Station.Id,
+    status: Station.Wr.CommandResponseCode,
 };
