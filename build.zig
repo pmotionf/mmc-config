8bd902c286413e1b570d52d228f1a3ec60e6b511
aaumar25 <aaumar@pmotionf.com>
2025-04-18 17:29:31 +0900

    feat: protobuf implementation
    refactor: change protobuf source
    
    scm: protobuf
    
    refactor: mmc-config dependency
    
    refactor: trying to fix the dependency
    
    fix protobuf
    
    fix target and optimize options
    
    refactor: public protobuf
    
    refactor: optional `SendCommand.GetStatus.Hall.axis_idx`

--- build.zig
+++ build.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const protobuf = @import("protobuf");
 
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
@@ -25,11 +26,24 @@ pub fn build(b: *std.Build) !void {
         .mdfunc_mock = mdfunc_mock_build,
     });
 
-    const mmc_config = b.addModule("mmc-config", .{
-        .root_source_file = b.path("src/mmc-config.zig"),
+    // first create a build for the dependency
+    const protobuf_dep = b.dependency("protobuf", .{
+        .target = target,
+        .optimize = optimize,
     });
 
-    mmc_config.addImport("mcl", mcl.module("mcl"));
+    _ = b.addModule("mmc-config", .{
+        .root_source_file = b.path("src/mmc-config.zig"),
+        .imports = &.{
+            .{ .name = "mcl", .module = mcl.module("mcl") },
+            .{ .name = "protobuf", .module = protobuf_dep.module("protobuf") },
+        },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // mmc_config.addImport("mcl", mcl.module("mcl"));
+    // mmc_config.addImport("protobuf", protobuf_dep.module("protobuf"));
 
     const mcl_mock = b.dependency("mcl", .{
         .target = target,
@@ -45,6 +59,7 @@ pub fn build(b: *std.Build) !void {
     });
 
     unit_tests.root_module.addImport("mcl", mcl_mock.module("mcl"));
+    unit_tests.root_module.addImport("protobuf", protobuf_dep.module("protobuf"));
 
     const run_unit_tests = b.addRunArtifact(unit_tests);
     const test_step = b.step("test", "Run unit tests");
@@ -53,4 +68,22 @@ pub fn build(b: *std.Build) !void {
     // Check step is same as test, as there is no output artifact.
     const check = b.step("check", "Check if foo compiles");
     check.dependOn(&run_unit_tests.step);
+
+    const gen_proto = b.step("gen-proto", "generates zig files from protocol buffer definitions");
+
+    const protoc_step = protobuf.RunProtocStep.create(
+        b,
+        protobuf_dep.builder,
+        target,
+        .{
+            // out directory for the generated zig files
+            .destination_directory = b.path("src/proto"),
+            .source_files = &.{
+                "protocol/all.proto",
+            },
+            .include_directories = &.{},
+        },
+    );
+
+    gen_proto.dependOn(&protoc_step.step);
 }
