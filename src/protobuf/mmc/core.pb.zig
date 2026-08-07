4c95217bfb6f036b245648146dc597c4eb44145f
Abdul Aris Umar <aaumar@premorobo.com>
2026-08-07 12:31:40 +0900

    refactor: Remove carrier width from track config (#84)

--- src/protobuf/mmc/core.pb.zig
+++ src/protobuf/mmc/core.pb.zig
@@ -126,7 +126,6 @@ pub const Response = struct {
             axis_length: f32 = 0,
             carrier_length: f32 = 0,
             drivers: u32 = 0,
-            carrier_width: f32 = 0,
 
             pub const _desc_table = .{
                 .id = fd(1, .{ .scalar = .uint32 }),
@@ -135,7 +134,6 @@ pub const Response = struct {
                 .axis_length = fd(4, .{ .scalar = .float }),
                 .carrier_length = fd(5, .{ .scalar = .float }),
                 .drivers = fd(6, .{ .scalar = .uint32 }),
-                .carrier_width = fd(7, .{ .scalar = .float }),
             };
 
             /// Encodes the message to the writer
