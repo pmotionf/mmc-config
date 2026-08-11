da694e4bc14d439fe776cfde6c2a5858624f2e3c
aaumar25 <aaumar@premorobo.com>
2026-08-11 16:08:23 +0900

    scm: protobuf 2.1.0

--- src/api.zig
+++ src/api.zig
@@ -5,7 +5,7 @@ pub const protobuf = struct {
     pub const root = @import("protobuf/root.pb.zig");
     pub const mmc = @import("protobuf/mmc.pb.zig");
 
-    pub const version = std.SemanticVersion.parse("2.0.0") catch unreachable;
+    pub const version = std.SemanticVersion.parse("2.1.0") catch unreachable;
 };
 
 pub const cclink = @import("cclink.zig");
